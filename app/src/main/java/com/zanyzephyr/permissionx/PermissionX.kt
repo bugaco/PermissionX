780e4d022b6d32c51a9c8060733e45c4adeeccb5
李懿哲 <liyizhezhe@gmail.com>
2020-05-16 21:41:10 +0800

    first commit

---
+++ app/src/main/java/com/zanyzephyr/permissionx/PermissionX.kt
@@ -0,0 +1,21 @@
+package com.zanyzephyr.permissionx
+
+import androidx.fragment.app.FragmentActivity
+
+object PermissionX {
+
+    private const val TAG = "InvisibleFragment"
+
+    fun request(activity: FragmentActivity, vararg permissions: String, callback: PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existFragment != null) {
+            existFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.request(callback, *permissions)
+    }
+}
